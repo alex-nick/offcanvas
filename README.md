9a8da473b62fe9189d3527a4cfc0aab39ba99d36
alex-nick <modemdriver@gmail.com>
2025-02-21 15:36:25 +0200

    indent_size

--- README.md
+++ README.md
@@ -1,9 +1,5 @@
-# MultiLevelPushMenu
+# Multi Level Offcanvas Menu
 
-An experimental push menu with multi-level functionality that allows endless nesting of navigation elements.
-
-[article on Codrops](http://tympanus.net/codrops/?p=16252)
+An offcanvas menu with multi-level functionality that allows endless nesting of navigation elements.
 
 [demo](http://tympanus.net/Development/MultiLevelPushMenu/)
-
-[LICENSING & TERMS OF USE](http://tympanus.net/codrops/licensing/)
--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,19 @@
-(function (window) {
+( function ( window ) {
 	'use strict';
 
-	function extend(a, b) {
-		return Object.assign(a, b);
+	function extend( a, b ) {
+		return Object.assign( a, b );
 	}
 
 	// returns the depth of the element "e" relative to element with id=id
 	// for this calculation only parents with classname = waypoint are considered
-	function getLevelDepth(e, id, waypoint) {
+	function getLevelDepth( e, id, waypoint ) {
 		let depthCount = 0;
-		while (e) {
-			if (e.id.indexOf(id) >= 0) {
+		while ( e ) {
+			if ( e.id.indexOf( id ) >= 0 ) {
 				return depthCount;
 			}
-			if (e.classList.contains(waypoint)) {
+			if ( e.classList.contains( waypoint ) ) {
 				depthCount++;
 			}
 			e = e.parentNode;
@@ -22,8 +22,8 @@
 	}
 
 	// returns the closest element to 'e' that has class "classname"
-	function closest(e, classname) {
-		return e.closest('.' + classname);
+	function closest( e, classname ) {
+		return e.closest( '.' + classname );
 	}
 
 	const MENU_ID = 'mp-menu';
@@ -46,10 +46,10 @@
 	 * @param {Element} trigger - The menu trigger element.
 	 * @param {Object}  options - The options object.
 	 */
-	function mlPushMenu(element, trigger, options) {
+	function mlPushMenu( element, trigger, options ) {
 		this.element = element;
 		this.trigger = trigger;
-		this.options = extend(this.defaults, options);
+		this.options = extend( this.defaults, options );
 		this._init();
 	}
 
@@ -62,20 +62,24 @@
 		},
 		_init() {
 			this.level = 0;
-			this.menu = document.getElementById(MENU_ID);
-			this.levels = [...this.element.getElementsByClassName(CLASS_LEVEL)];
+			this.menu = document.getElementById( MENU_ID );
+			this.levels = [
+				...this.element.getElementsByClassName( CLASS_LEVEL ),
+			];
 			// save the depth of each of these mp-level elements
-			this.levels.forEach((element) => {
+			this.levels.forEach( ( element ) => {
 				element.setAttribute(
 					'data-level',
-					getLevelDepth(element, this.element.id, CLASS_LEVEL)
+					getLevelDepth( element, this.element.id, CLASS_LEVEL )
 				);
-			});
+			} );
 			// the menu items
-			this.menuItems = [...this.element.getElementsByTagName('li')];
+			this.menuItems = [ ...this.element.getElementsByTagName( 'li' ) ];
 			// if type == "cover" these will serve as hooks to move back to the previous level
 			this.levelBack = [
-				...this.element.getElementsByClassName(this.options.backClass),
+				...this.element.getElementsByClassName(
+					this.options.backClass
+				),
 			];
 			// event type (if mobile use touch events)
 			// this.eventtype = mobilecheck() ? 'touchstart' : 'click';
@@ -87,113 +91,113 @@
 			const self = this;
 
 			// the menu should close if clicking somewhere on the body
-			const bodyClick = (element) => {
+			const bodyClick = ( element ) => {
 				self._resetMenu();
-				element.removeEventListener(self.eventtype, bodyClick);
+				element.removeEventListener( self.eventtype, bodyClick );
 			};
 
 			// open (or close) the menu
-			this.trigger.addEventListener(this.eventtype, function (event) {
+			this.trigger.addEventListener( this.eventtype, function ( event ) {
 				event.stopPropagation();
 				event.preventDefault();
-				if (self.open) {
+				if ( self.open ) {
 					self._resetMenu();
 				} else {
 					self._openMenu();
 					// the menu should close if clicking somewhere on the body (excluding clicks on the menu)
 					document.addEventListener(
 						self.eventtype,
-						function (newEvent) {
+						function ( newEvent ) {
 							if (
 								self.open &&
-								!newEvent.target.closest(self.element.id)
+								! newEvent.target.closest( self.element.id )
 							) {
-								bodyClick(this);
+								bodyClick( this );
 							}
 						}
 					);
 				}
-			});
+			} );
 
 			// opening a sub level menu
-			this.menuItems.forEach((menuItem) => {
+			this.menuItems.forEach( ( menuItem ) => {
 				// check if it has a sub level
-				const subLevel = menuItem.querySelector(`.${CLASS_LEVEL}`);
-				if (subLevel) {
+				const subLevel = menuItem.querySelector( `.${ CLASS_LEVEL }` );
+				if ( subLevel ) {
 					menuItem
-						.querySelector('a')
-						.addEventListener(self.eventtype, function (event) {
+						.querySelector( 'a' )
+						.addEventListener( self.eventtype, function ( event ) {
 							event.preventDefault();
 							const level = closest(
 								menuItem,
 								CLASS_LEVEL
-							).getAttribute('data-level');
-							if (self.level <= level) {
+							).getAttribute( 'data-level' );
+							if ( self.level <= level ) {
 								event.stopPropagation();
-								closest(menuItem, CLASS_LEVEL).classList.add(
+								closest( menuItem, CLASS_LEVEL ).classList.add(
 									CLASS_OVERLAY
 								);
-								self._openMenu(subLevel);
+								self._openMenu( subLevel );
 							}
-						});
+						} );
 				}
-			});
+			} );
 
 			// closing the sub levels :
 			// by clicking on the visible part of the level element
-			this.levels.forEach((element) => {
+			this.levels.forEach( ( element ) => {
 				element.setAttribute(
 					'data-level',
-					getLevelDepth(element, self.element.id, CLASS_LEVEL)
+					getLevelDepth( element, self.element.id, CLASS_LEVEL )
 				);
-				element.addEventListener(self.eventtype, function (event) {
+				element.addEventListener( self.eventtype, function ( event ) {
 					event.stopPropagation();
-					const level = element.getAttribute('data-level');
-					if (self.level > level) {
+					const level = element.getAttribute( 'data-level' );
+					if ( self.level > level ) {
 						self.level = level;
 						self._closeMenu();
 					}
-				});
-			});
+				} );
+			} );
 
 			// by clicking on a specific element
-			this.levelBack.forEach(function (element) {
-				element.addEventListener(self.eventtype, function (event) {
+			this.levelBack.forEach( function ( element ) {
+				element.addEventListener( self.eventtype, function ( event ) {
 					event.preventDefault();
-					const level = closest(element, CLASS_LEVEL).getAttribute(
+					const level = closest( element, CLASS_LEVEL ).getAttribute(
 						'data-level'
 					);
 
-					if (self.level <= level) {
+					if ( self.level <= level ) {
 						event.stopPropagation();
 						self.level =
-							closest(element, CLASS_LEVEL).getAttribute(
+							closest( element, CLASS_LEVEL ).getAttribute(
 								'data-level'
 							) - 1;
 						return self.level === 0
 							? self._resetMenu()
 							: self._closeMenu();
 					}
-				});
-			});
+				} );
+			} );
 		},
 
-		_openMenu(subLevel) {
+		_openMenu( subLevel ) {
 			// increment level depth
 			++this.level;
 
 			// move the main wrapper
-			const levelFactor = (this.level - 1) * this.options.levelSpacing;
-			this._setTransform(`translate3d(-${levelFactor}px,0,0)`);
+			const levelFactor = ( this.level - 1 ) * this.options.levelSpacing;
+			this._setTransform( `translate3d(-${ levelFactor }px,0,0)` );
 
-			if (subLevel) {
+			if ( subLevel ) {
 				// reset transform for sublevel
-				this._setTransform('', subLevel);
+				this._setTransform( '', subLevel );
 				// need to reset the translate value for the level menus that have the same level depth and are not open
-				for (const levelEl of this.levels) {
+				for ( const levelEl of this.levels ) {
 					if (
 						levelEl !== subLevel &&
-						!levelEl.classList.contains(CLASS_OPEN)
+						! levelEl.classList.contains( CLASS_OPEN )
 					) {
 						this._setTransform(
 							'translate3d(100%,0,0) translate3d(-' +
@@ -205,41 +209,43 @@
 				}
 			}
 			// add class mp-pushed to main wrapper if opening the first time
-			if (this.level === 1) {
-				this.menu.classList.add(CLASS_PUSHED);
+			if ( this.level === 1 ) {
+				this.menu.classList.add( CLASS_PUSHED );
 				this.open = true;
 			}
 			// add class mp-level-open to the opening level element
-			(subLevel || this.levels[0]).classList.add(CLASS_OPEN);
+			( subLevel || this.levels[ 0 ] ).classList.add( CLASS_OPEN );
 		},
 		// close the menu
 		_resetMenu() {
-			this._setTransform('translate3d(100%,0,0)');
+			this._setTransform( 'translate3d(100%,0,0)' );
 			this.level = 0;
 			// remove class mp-pushed from main wrapper
-			this.menu.classList.remove(CLASS_PUSHED);
+			this.menu.classList.remove( CLASS_PUSHED );
 			this._toggleLevels();
 			this.open = false;
 		},
 		// close sub menus
 		_closeMenu() {
-			const levelFactor = (this.level - 1) * this.options.levelSpacing;
-			this._setTransform(`translate3d(-${levelFactor}px,0,0)`);
+			const levelFactor = ( this.level - 1 ) * this.options.levelSpacing;
+			this._setTransform( `translate3d(-${ levelFactor }px,0,0)` );
 			this._toggleLevels();
 		},
 		// translate the element
-		_setTransform(value, element = this.menu) {
-			requestAnimationFrame(() => {
+		_setTransform( value, element = this.menu ) {
+			requestAnimationFrame( () => {
 				element.style.transform = value;
-			});
+			} );
 		},
 		// removes classes mp-level-open from closing levels
 		_toggleLevels() {
-			for (const levelEl of this.levels) {
-				if (levelEl.getAttribute('data-level') >= this.level + 1) {
-					levelEl.classList.remove(CLASS_OPEN, CLASS_OVERLAY);
-				} else if (levelEl.getAttribute('data-level') === this.level) {
-					levelEl.classList.remove(CLASS_OVERLAY);
+			for ( const levelEl of this.levels ) {
+				if ( levelEl.getAttribute( 'data-level' ) >= this.level + 1 ) {
+					levelEl.classList.remove( CLASS_OPEN, CLASS_OVERLAY );
+				} else if (
+					levelEl.getAttribute( 'data-level' ) === this.level
+				) {
+					levelEl.classList.remove( CLASS_OVERLAY );
 				}
 			}
 		},
@@ -247,4 +253,4 @@
 
 	// add to global namespace
 	window.mlPushMenu = mlPushMenu;
-})(window);
+} )( window );
